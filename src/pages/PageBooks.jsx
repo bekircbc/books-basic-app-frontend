a707514dfe8e9d3c40d7c63f6db731d4e7d21fbc
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-14 17:01:10 +0200

    data come from port 4433

--- src/pages/PageBooks.jsx
+++ src/pages/PageBooks.jsx
@@ -1,43 +1,19 @@
-import { useFetch } from '../hooks/useFetch';
+import { useFetch } from "../hooks/useFetch";
 
 export const PageBooks = () => {
-	const { items: techBooks, itemTotal: totalBooks } = useFetch(
-		'https://edwardtanguay.netlify.app/share/techBooks.json'
-	);
-	const { items: employees, isLoading: employeesAreLoading } = useFetch(
-		'https://edwardtanguay.netlify.app/share/employees.json',
-		true
-	);
-	const { itemTotal: totalFlashcards, isLoading: flashcardsAreLoading } =
-		useFetch(
-			'https://edwardtanguay.netlify.app/share/flashcards.json',
-			true
-		);
+  const { items, itemTotal } = useFetch("http://localhost:4433/");
 
-	return (
-		<div className="page_books">
-			<p>
-				{employeesAreLoading ? (
-					<p>Employees are loading...</p>
-				) : (
-					<p>
-						Note: There are {employees.length} employees who have
-						access to this page.
-					</p>
-				)}
-			</p>
-			{flashcardsAreLoading ? (
-				<p>Flashcards are loading...</p>
-			) : (
-				<p>There are {totalFlashcards} flashcards about books.</p>
-			)}
-			<h2>There are {totalBooks} tech books.</h2>
+  console.log(items, itemTotal);
 
-			<ul>
-				{techBooks.map((techBook, index) => {
-					return <li key={index}>{techBook.title}</li>;
-				})}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="page_books">
+      <h2>There are {itemTotal} books.</h2>
+
+      <ul>
+        {items.map((book) => {
+          return <li key={book.id}>{book.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
