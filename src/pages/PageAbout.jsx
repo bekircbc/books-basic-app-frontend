e4afbed12ddaa6ae21ffcd5349810205d21f81ce
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-15 08:43:04 +0200

    main goal of this page has been changed. this can be a library in the future.

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -1,8 +1,11 @@
 export const PageAbout = () => {
-	return (
-		<div className="page_about">
-			<h2>About</h2>
-			<p>This is some info about the site.</p>
-		</div>
-	);
+  return (
+    <div className="page_about">
+      <h2>About</h2>
+      <p>
+        This page is created for custom hooks, Node/Express Server. In the
+        future can be a Library for tools and custom hooks in React.
+      </p>
+    </div>
+  );
 };
